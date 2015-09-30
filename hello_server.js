545d79deb6e659c86215cf367a16b08387799053
Michelle Burton <MacMini@Michelles-Mac-mini.local>
2015-09-29 22:21:11 -0500

    jedi Name added, first 3 letters of last name, followed by first 3 letters of first name

--- hello_server.js
+++ hello_server.js
@@ -3,8 +3,16 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response){
-  response.send("Hello World");
+  response.send("Hello World!");
 });
 
+app.get('/:firstname/:lastname', function(request, response){
+    var first = (request.params.firstname).substring( 0, 3 );
+    var last = (request.params.lastname).substring( 0, 3 );
+    response.send("hello " + last +first);
 
-app.listen(process.env.Port, process.env.IP);
+});
+
+app.listen(8080);
+
+//app.listen(process.env.PORT, process.env.IP);
